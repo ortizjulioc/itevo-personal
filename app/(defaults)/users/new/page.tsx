00bec59d67991fa49899106611a135540b5d1602
Wilber Galvez <galvezwilber22@gmail.com>
2024-09-17 14:22:56 -0400

    [add] pagina new usuarios

--- app/(defaults)/users/new/page.tsx
+++ app/(defaults)/users/new/page.tsx
@@ -1,5 +1,5 @@
+'use client'
 import { Suspense } from 'react';
-
 import { Metadata } from 'next';
 import ViewTitle from '@/components/common/ViewTitle';
 import IconUserPlus from '@/components/icon/icon-user-plus';
@@ -7,20 +7,14 @@ import Button from '@/components/ui/button';
 import Link from 'next/link';
 import UserForm from '../components/user-form';
 
-export const metadata: Metadata = {
-    title: 'Nuevo Usuario',
-};
 
 export default async function NewUser() {
     return (
-        <div>
-            <div className="flex flex-wrap items-center justify-between gap-4">
-                <h2 className="text-xl">Usuarios</h2>
-
-                <Suspense fallback={<div>Loading...</div>}>
-                   <UserForm />
-                </Suspense>
-            </div>
+        <div className="flex flex-col gap-4">
+             <ViewTitle title="Crear Nuevo Usuario" showBackPage={true} />
+            <Suspense fallback={<div>Loading...</div>}>
+                <UserForm />
+            </Suspense>
         </div>
     );
 }
