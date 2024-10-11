6581166f9161cde99fc24870644cf2730e00ad4f
Wilber Galvez <galvezwilber22@gmail.com>
2024-10-11 17:11:42 -0400

    [add] request para asignar y colocar permisos

--- app/(defaults)/users/lib/request.ts
+++ app/(defaults)/users/lib/request.ts
@@ -1,19 +1,32 @@
-import apiRequest from "@/utils/lib/api-request/request";
-import { User } from "@prisma/client";
+import apiRequest from '@/utils/lib/api-request/request';
+import { User } from '@prisma/client';
 
 export interface UserResponse {
     users: User[];
     totalUsers: number;
 }
+export interface AssingRole {
+    userId: string;
+    branchId: string;
+    roleId: string;
+}
 
 export const createUser = async (user: User) => {
     return await apiRequest.post<User>('/users', user);
-}
+};
 
 export const updateUser = async (id: string, user: User) => {
     return await apiRequest.put<User>(`/users/${id}`, user);
-}
+};
 
 export const deleteUser = async (id: string) => {
     return await apiRequest.remove<string>(`/users/${id}`);
+};
+
+export const AssingRole = async (data: AssingRole) => {
+    return await apiRequest.post<AssingRole>(`/api/authorization/assign-role`, data);
+};
+
+export const RemoveRole = async (data: AssingRole) => {
+    return await apiRequest.remove<AssingRole>(`/api/authorization/remove-role`, { data });
 }
