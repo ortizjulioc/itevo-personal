5d61c399e1af36f2127089335b381b6b64e94869
Wilber Galvez <galvezwilber22@gmail.com>
2024-10-14 14:45:51 -0400

    [add] componet branch options

--- app/(defaults)/users/components/user-forms/update-form/BranchesOption.tsx
+++ app/(defaults)/users/components/user-forms/update-form/BranchesOption.tsx
@@ -1,41 +1,40 @@
 import Card from "@/components/common/Card";
-import Switcher from "@/components/common/Switcher";
+
 import classNames from "classnames";
+import { Branch } from "@prisma/client";
+import Switcher from "@/components/common/Switcher";
+
 
 // import RoleModal from "./RoleModal";
 
-interface App {
-  id: number;
-  code: string;
-  name: string;
-}
 
-interface AppsOptionsProps {
-  app: App;
+
+interface BranchesOptionsProps {
+  branch: Branch;
   className?: string;
-  onChange: (checked: boolean, id: number) => void;
-  loading: boolean;
+  onChange: (checked: boolean, id: string) => void;
+  loading: any;
   active: boolean;
 }
 
-const BranchesOption: React.FC<AppsOptionsProps> = ({ app, className, onChange, loading, active }) => {
-  const { id, code, name } = app;
+const BranchesOption: React.FC<BranchesOptionsProps> = ({ branch, className, onChange, loading, active }) => {
+  const { id, name, address } = branch
 
   return (
     <div className={classNames('panel', className)}>
       <div className='flex flex-col justify-between h-full'>
         <div className='flex flex-col justify-between mb-4'>
           <div className="flex justify-between items-center">
-            <span className='text-base font-bold text-gray-900'>{code}</span>
+            <span className='text-base font-bold text-gray-900'>{name}</span>
             <div>
               <Switcher
+                id={id.toString()}
                 checked={active}
                 onChange={(checked) => onChange(checked, id)}
-                isLoading={loading}
               />
             </div>
           </div>
-          <span>{name}</span>
+          <span>{address}</span>
         </div>
         {/* <RoleModal app={app} /> */}
       </div>
--- app/(defaults)/users/components/user-forms/update-form/authorization-fields.tsx
+++ app/(defaults)/users/components/user-forms/update-form/authorization-fields.tsx
@@ -1,33 +1,28 @@
 import { Field } from "formik";
 import { FormItem, Input } from "@/components/ui";
 import BranchesOption from "./BranchesOption";
+import useFetchBranch from "../../../lib/use-fetch-branches";
 
-export default function AuthorizationFields({ errors, touched }: any) {
+export default function AuthorizationFields({values, errors, touched }: any) {
+
+      console.log(values.branches);
+  const { loading, error, branches, totalBranches, setBranches } = useFetchBranch('');
     return (
         <>
           <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4">
-           <BranchesOption
-           key={'111'}
-           app={{id: 1, name: 'Santo Domingo',code: 'SANTO DOMINGO'}}
-           onChange={() => {}}
-           loading={false}
-           active={true}
-            />
+          {branches?.map((branch) => {
+            return (
               <BranchesOption
-              key={'222'}
-                app={{id: 2, name: 'San Francisco de Macoris',code: 'SAN FRANCISCO'}}
-                onChange={() => {}}
-                loading={false}
-                active={true}
-                />
-
-                <BranchesOption
-                key={'333'}
-                app={{id: 3, name: 'Nagua',code: 'NAGUA'}}
-                onChange={() => {}}
-                loading={false}
-                active={true}
-                />
+              key={branch.id}
+              branch={branch}
+              onChange={() => {}}
+              loading={false}
+              active={values.branches?.includes(branch.id)}
+              />
+            );
+          }
+          )}
+             
             </div>
         </>
     )
