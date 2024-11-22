49d6d47cba79de880036b7bbc07fc04cecae0598
Wilber Galvez <galvezwilber22@gmail.com>
2024-11-22 17:05:46 -0400

    [add] student forms

---
+++ app/(defaults)/students/components/student-form/form.config.ts
@@ -0,0 +1,34 @@
+
+import * as Yup from 'yup';
+
+
+export const createValidationSchema = Yup.object().shape({
+    firstName: Yup.string().required('El nombre es obligatorio'),
+    lastName: Yup.string().required('El apellido es obligatorio'),
+    identification: Yup.string().required('La identificación es obligatoria'),
+    address: Yup.string().required('La dirección es obligatoria'),
+    phone: Yup.string().required('El teléfono es obligatorio'),
+    email: Yup.string().email('El correo electrónico no es válido').required('El correo electrónico es obligatorio'),
+    hasTakenCourses: Yup.boolean().required('Este campo es obligatorio'),
+   
+});
+
+export const updateValidationSchema = Yup.object().shape({
+    firstName: Yup.string().required('El nombre es obligatorio'),
+    lastName: Yup.string().required('El apellido es obligatorio'),
+    identification: Yup.string().required('La identificación es obligatoria'),
+    address: Yup.string().required('La dirección es obligatoria'),
+    phone: Yup.string().required('El teléfono es obligatorio'),
+    email: Yup.string().email('El correo electrónico no es válido').required('El correo electrónico es obligatorio'),
+    hasTakenCourses: Yup.boolean().required('Este campo es obligatorio'),
+});
+
+export const initialValues = {
+    firstName: '',
+    lastName: '',
+    identification: '',
+    address: '',
+    phone: '',
+    email: '',
+    hasTakenCourses: false,
+};
