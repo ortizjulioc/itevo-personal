9e039c0d27cffecc5a9388f8aaf42cc813f3c69b
Wilber Galvez <galvezwilber22@gmail.com>
2025-01-19 09:22:39 -0400

    [update] actualizando la vista de  settings a tab

--- app/(defaults)/settings/page.tsx
+++ app/(defaults)/settings/page.tsx
@@ -1,9 +1,9 @@
 'use client';
-import { FormSkeleton, ViewTitle } from "@/components/common";
-
+import { ViewTitle } from "@/components/common";
+import { Tab } from "@headlessui/react";
 import useFetchSetting from "./lib/use-fetch-settings";
 import UpdateSettingForm from "./components/setting-form/update-form";
-
+import { Fragment } from "react";
 
 
 export default function EditSetting({ params }: { params: { id: string } }) {
@@ -11,10 +11,43 @@ export default function EditSetting({ params }: { params: { id: string } }) {
     const { loading, Setting } = useFetchSetting();
     return (
         <div>
-            <ViewTitle className='mb-6' title="Configuracion de la Empresa"  />
+            <ViewTitle className='mb-6' title="Configuracion de la Empresa" />
 
-            {/* {loading && <FormSkeleton />} */}
-            {Setting && <UpdateSettingForm initialValues={Setting} />}
+            <Tab.Group>
+                <Tab.List className="mt-3 flex flex-wrap">
+                    <Tab as={Fragment}>
+                        {({ selected }) => (
+                            <button
+                                className={`${selected ? 'text-secondary !outline-none before:!w-full' : ''} relative -mb-[1px] flex items-center p-5 py-3 before:absolute before:bottom-0 before:left-0 before:right-0 before:m-auto before:inline-block before:h-[1px] before:w-0 before:bg-secondary before:transition-all before:duration-700 hover:text-secondary hover:before:w-full`}
+                            >
+                                Información general
+                            </button>
+                        )}
+                    </Tab>
+                    <Tab as={Fragment}>
+                        {({ selected }) => (
+                            <button
+                                className={`${selected ? 'text-secondary !outline-none before:!w-full' : ''} relative -mb-[1px] flex items-center p-5 py-3 before:absolute before:bottom-0 before:left-0 before:right-0 before:m-auto before:inline-block before:h-[1px] before:w-0 before:bg-secondary before:transition-all before:duration-700 hover:text-secondary hover:before:w-full`}
+                            >
+                                Horarios de Clase
+                            </button>
+                        )}
+                    </Tab>
+                </Tab.List>
+
+                <Tab.Panels>
+                    <Tab.Panel>
+                        <div className="mt-6">
+                            {Setting && <UpdateSettingForm initialValues={Setting} />}
+                        </div>
+                    </Tab.Panel>
+                    <Tab.Panel>
+                        <div className="mt-6">
+                            {Setting && <UpdateSettingForm initialValues={Setting} />}
+                        </div>
+                    </Tab.Panel>
+                </Tab.Panels>
+            </Tab.Group>
         </div>
     )
 }
