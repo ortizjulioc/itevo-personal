969b5db8d5ad111aa05700737965dcdb1c78f8e9
Jeffry Sanchez <jeffrycoder@gmail.com>
2025-01-24 09:36:30 -0400

    [UPDATE] - Se ha implementado la funcion formatErrorMessage en los catch del CRUD course branch

--- app/api/course-branch/[id]/route.ts
+++ app/api/course-branch/[id]/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { findCourseBranchById, updateCourseBranchById, deleteCourseBranchById } from '@/services/course-branch-service';
 import { validateObject } from '@/utils';
+import { formatErrorMessage } from '@/utils/error-to-string';
 
 // Obtener courseBranch por ID
 export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
@@ -15,11 +16,7 @@ export async function GET(request: NextRequest, { params }: { params: { id: stri
 
         return NextResponse.json(courseBranch, { status: 200 });
     } catch (error) {
-        if (error instanceof Error) {
-            return NextResponse.json({ code: 'E_SERVER_ERROR', message: 'Error buscando el curso', details: error.message }, { status: 500 });
-        } else {
-            return NextResponse.json(error, { status: 500 });
-        }
+        return NextResponse.json({ error: formatErrorMessage(error)},{ status: 500});
     }
 }
 
@@ -46,11 +43,7 @@ export async function PUT(request: NextRequest, { params }: { params: { id: stri
 
         return NextResponse.json(updatedCourseBranch, { status: 200 });
     } catch (error) {
-        if (error instanceof Error) {
-            return NextResponse.json({ code: 'E_SERVER_ERROR', message: 'Error actualizando el course branch', details: error.message }, { status: 500 });
-        } else {
-            return NextResponse.json(error, { status: 500 });
-        }
+        return NextResponse.json({ error: formatErrorMessage(error)},{ status: 500});
     }
 }
 
@@ -70,10 +63,6 @@ export async function DELETE(request: NextRequest, { params }: { params: { id: s
 
         return NextResponse.json({ message: 'courseBranch eliminado correctamente' });
     } catch (error) {
-        if (error instanceof Error) {
-            return NextResponse.json({ code: 'E_SERVER_ERROR', message: 'Error eliminando el courseBranch', details: error.message }, { status: 500 });
-        } else {
-            return NextResponse.json(error, { status: 500 });
-        }
+        return NextResponse.json({ error: formatErrorMessage(error)},{ status: 500});
     }
 }
