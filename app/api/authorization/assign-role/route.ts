72b6f08edd439e4970fcefc127adce962782191f
Jeffry Sanchez <jeffrycoder@gmail.com>
2025-01-27 09:43:29 -0400

    [ADD] - Agregando log authorization

--- app/api/authorization/assign-role/route.ts
+++ app/api/authorization/assign-role/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse, NextRequest } from "next/server";
 import { assignRoleToUserInBranch } from "@/services/authorization-service";
 import { formatErrorMessage } from "@/utils/error-to-string";
+import { createLog } from "@/utils/log";
 
 export async function POST(request: NextRequest) {
     try {
@@ -13,8 +14,27 @@ export async function POST(request: NextRequest) {
 
         const result = await assignRoleToUserInBranch(userId, branchId, roleId);
 
+
+        // Enviar log de auditoría
+        await createLog({
+            action: "POST",
+            description: `Se asignó un rol a un usuario con los siguientes datos: ${JSON.stringify(body, null, 2)}`,
+            origin: "authorization/assign-role",
+            elementId: userId,
+            success: true,
+        });
         return NextResponse.json(result, { status: 201 });
     } catch (error) {
+
+        // Enviar log de auditoría
+
+        await createLog({
+            action: "POST",
+            description: `Error al asignar un rol a un usuario: ${formatErrorMessage(error)}`,
+            origin: "authorization/assign-role",
+            elementId: request.headers.get("origin") || "",
+            success: false,
+        });
         return NextResponse.json({ error: formatErrorMessage(error)},{ status: 500});
     }
 }
