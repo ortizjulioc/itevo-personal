713b10223a15e5dd68faa2d763d23745dd06a7b1
Wilber Galvez <galvezwilber22@gmail.com>
2025-02-05 10:38:00 -0400

    [update] cambiando el estado por el enum

---
+++ app/(defaults)/course-branch/[id]/page.tsx
@@ -0,0 +1,24 @@
+'use client';
+import { FormSkeleton, ViewTitle } from "@/components/common";
+import { useFetchCourseBranchById } from "../lib/use-fetch-course-branch";
+import UpdateCourseBranchForm from "../components/course-branch-form/update-form";
+
+
+
+
+
+export default function EditCourseBranch({ params }: { params: { id: string } }) {
+    const { id } = params;
+    const { loading, courseBranch } = useFetchCourseBranchById(id);
+    console.log('courseBranch', courseBranch);
+   
+    return (
+        <div>
+            <ViewTitle className='mb-6' title="Editar Curso" showBackPage />
+            
+
+            {loading && <FormSkeleton />}
+            {courseBranch && <UpdateCourseBranchForm initialValues={courseBranch} />}
+        </div>
+    )
+}
