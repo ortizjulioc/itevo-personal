cf07fa747004d46b54a673789f000cd188e96f14
Wilber Galvez <galvezwilber22@gmail.com>
2025-02-05 10:44:57 -0400

    [update] cambiando waiting en string por el enum de waiting

--- app/api/course-branch/route.ts
+++ app/api/course-branch/route.ts
@@ -1,9 +1,9 @@
-import { NextResponse, NextRequest } from "next/server";
-import { validateObject } from "@/utils";
-import { getCourseBranch, createCourseBranch } from "@/services/course-branch-service";
-import { formatErrorMessage } from "@/utils/error-to-string";
-import { createLog } from "@/utils/log";
-import { CourseBranch } from "@prisma/client";
+import { NextResponse, NextRequest } from 'next/server';
+import { validateObject } from '@/utils';
+import { getCourseBranch, createCourseBranch } from '@/services/course-branch-service';
+import { formatErrorMessage } from '@/utils/error-to-string';
+import { createLog } from '@/utils/log';
+import { CourseBranch, CourseBranchStatus, Modality } from '@prisma/client';
 
 export async function GET(request: NextRequest) {
     try {
@@ -14,32 +14,35 @@ export async function GET(request: NextRequest) {
 
         const { courseBranches, totalCourseBranches } = await getCourseBranch(search, page, top);
 
-        return NextResponse.json({
-            courseBranches,
-            totalCourseBranches,
-        }, { status: 200 });
+        return NextResponse.json(
+            {
+                courseBranches,
+                totalCourseBranches,
+            },
+            { status: 200 }
+        );
     } catch (error) {
-        return NextResponse.json({ error: formatErrorMessage(error)},{ status: 500});
+        return NextResponse.json({ error: formatErrorMessage(error) }, { status: 500 });
     }
 }
 
 export async function POST(request: Request) {
     try {
         const body = await request.json();
-        console.log('BODY: ',body);
-        body.status = 'waiting';
-
+        console.log('BODY: ', body);
+        body.status = CourseBranchStatus.WAITING;
+      
         // Validate the request body
-        const {isValid, message} = validateObject(body, ['promotionId', 'branchId', 'teacherId', 'courseId']);
+        const { isValid, message } = validateObject(body, ['promotionId', 'branchId', 'teacherId', 'courseId']);
         if (!isValid) {
             return NextResponse.json({ code: 'E_MISSING_FIELDS', error: message }, { status: 400 });
         }
 
         const courseBranch = await createCourseBranch(body);
         await createLog({
-            action: "POST",
+            action: 'POST',
             description: `Se creó un curso con los siguientes datos: ${JSON.stringify(body, null, 2)}`,
-            origin: "course-branch",
+            origin: 'course-branch',
             elementId: courseBranch.id,
             success: true,
         });
@@ -47,13 +50,13 @@ export async function POST(request: Request) {
         return NextResponse.json(courseBranch, { status: 201 });
     } catch (error) {
         await createLog({
-            action: "POST",
+            action: 'POST',
             description: `Error al crear un curso: ${formatErrorMessage(error)}`,
-            origin: "course-branch",
-            elementId: request.headers.get("origin") || "",
+            origin: 'course-branch',
+            elementId: request.headers.get('origin') || '',
             success: false,
         });
 
-        return NextResponse.json({ error: formatErrorMessage(error)},{ status: 500});
+        return NextResponse.json({ error: formatErrorMessage(error) }, { status: 500 });
     }
 }
