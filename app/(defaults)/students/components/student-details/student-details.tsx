8600bf69c8e85fca7d00cc48666c2381fd97e37d
Wilber Galvez <galvezwilber22@gmail.com>
2025-02-11 10:16:12 -0400

    [add] vista de detalles para el estudiante

---
+++ app/(defaults)/students/components/student-details/student-details.tsx
@@ -0,0 +1,77 @@
+import { IconEdit, IconTrashLines, IconUserPlus } from '@/components/icon';
+import { Button } from '@/components/ui';
+import Tooltip from '@/components/ui/tooltip';
+import { Student } from '@prisma/client';
+import Link from 'next/link';
+import React from 'react';
+
+export default function StudentDetails({ student }: { student: Student }) {
+
+    const onDelete = (id: string) => {
+        console.log('Deleting student', id);
+    };
+    return (
+        <div className="grid grid-cols-1 gap-4 p-6 bg-white dark:bg-gray-800 rounded-lg shadow-md">
+            {/* Nombre del estudiante */}
+            <h2 className="text-xl font-semibold text-center flex justify-center sm:text-left col-span-1 sm:col-span-2">
+                {student.firstName} {student.lastName}
+            </h2>
+
+            {[
+                { label: "Codigo", value: student.code },
+                { label: "Identificación", value: student.identification },
+                { label: "Teléfono", value: student.phone },
+                { label: "Correo", value: student.email },
+                { label: "Dirección", value: student.address },
+                { label: "Ha tomado cursos ", value: student.hasTakenCourses ? "Sí" : "No" },
+            ].map(({ label, value }) => (
+                <div key={label} className="flex flex-col md:col-span-2">
+                    <span className="font-bold">{label}</span>
+                    <span className="text-gray-700 dark:text-gray-200 font-semibold break-words">{value || "No especificado"}</span>
+                </div>
+            ))}
+
+            <div className="grid grid-cols-1 sm:grid-cols-3 col-span-2 gap-2">
+                <Tooltip title="Eliminar">
+                    <Button
+                        onClick={() => onDelete(student.id)}
+                        variant="outline"
+                        size="sm"
+                        icon={<IconTrashLines className="size-4" />}
+                        color="danger"
+                        className="w-full"
+                    >
+                        Eliminar
+                    </Button>
+                </Tooltip>
+                <Tooltip title="Editar">
+                    <Link href={`/students/${student.id}`}   className="w-full">
+                        <Button
+                            variant="outline"
+                            size="sm"
+                            icon={<IconEdit className="size-4" />}
+                            className="w-full"
+                        >
+                            Editar
+                        </Button>
+                    </Link>
+                </Tooltip>
+                <Tooltip title="Inscribir">
+                    <Link href={`/students/${student.id}`}   className="w-full">
+                        <Button
+                            variant="outline"
+                            size="sm"
+                            icon={<IconUserPlus className="size-4" />}
+                            className="w-full "
+                            color='success'
+                        >
+                            Inscribir
+                        </Button>
+                    </Link>
+                </Tooltip>
+            </div>
+        </div>
+
+
+    );
+}
---
+++ app/(defaults)/students/components/student-details/student-enrollments.tsx
@@ -0,0 +1,84 @@
+'use client';
+import { openNotification, queryStringToObject } from "@/utils";
+
+import Skeleton from "@/components/common/Skeleton";
+import StudentLabel from "@/components/common/info-labels/student-label";
+import CourseBranchLabel from "@/components/common/info-labels/course-branch-label";
+import { ENROLLMENT_STATUS } from "@/constants/enrollment.status.constant";
+import useFetchEnrollments from "@/app/(defaults)/enrollments/lib/use-fetch-enrollments";
+import { Pagination } from "@/components/ui";
+
+
+
+
+
+interface Props {
+    className?: string;
+    query?: string;
+}
+
+export default function StudentEnrollments({ className, query = '' }: Props) {
+    const params = queryStringToObject(query);
+    const { loading, error, enrollments, totalEnrollments, setEnrollments } = useFetchEnrollments(query);
+    if (error) {
+        openNotification('error', error);
+    }
+
+    const enrollmentStatus = [
+          { value: ENROLLMENT_STATUS.WAITING, label: 'En espera' },
+          { value: ENROLLMENT_STATUS.ENROLLED, label: 'Inscrito' },
+          { value: ENROLLMENT_STATUS.COMPLETED, label: 'Completado' },
+          { value: ENROLLMENT_STATUS.ABANDONED, label: 'Abandonado' },
+      ];
+  
+    if (loading) return <Skeleton rows={6} columns={['ESTUDIANTE','OFERTA ACADEMICA','FECHA DE INSCRIPCION']} />;
+
+    return (
+        <div className='col-span-2'>
+            <h1 className="text-2xl font-semibold dark:text-white-light mb-3">Inscripciones</h1>
+            <div className="table-responsive mb-5 panel p-0 border-0 overflow-hidden">
+                <table className="table-hover">
+                    <thead>
+                        <tr>
+                            <th>OFERTA ACADEMICA</th>
+                            <th>FECHA DE INSCRIPCION</th>
+                            <th>ESTADO</th>
+                            
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {enrollments?.length === 0 && (
+                            <tr>
+                                <td colSpan={6} className="text-center text-gray-500 dark:text-gray-600 italic">No se encontraron Inscripciones registradas</td>
+                            </tr>
+                        )}
+                        {enrollments?.map((enrollment) => {
+                            return (
+                                <tr key={enrollment.id}>
+                                    <td>
+                                        <CourseBranchLabel CourseBranchId={enrollment.courseBranchId} />
+                                    </td>
+                                    <td>
+                                        {new Date(enrollment.enrollmentDate).toLocaleDateString()}
+                                    </td>
+                                    <td>
+                                       {enrollmentStatus.find((status) => status.value === enrollment.status)?.label}
+                                    </td>
+                                   
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </table>
+
+            </div>
+            <div className="">
+                <Pagination
+                    currentPage={parseInt(params?.page || '1')}
+                    total={totalEnrollments}
+                    top={parseInt(params?.top || '10')}
+                />
+            </div>
+        </div>
+    );
+};
