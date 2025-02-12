5bf67d990560918a59d6e470268398d45cf520e7
Jeffry Sanchez <jeffrycoder@gmail.com>
2025-02-12 11:16:30 -0400

    [ADD] - Agregando filtro por enrollmentDate

--- services/enrollment-service.ts
+++ services/enrollment-service.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from "@prisma/client";
 import { equal } from 'assert';
 const Prisma = new PrismaClient();
 
-export const getEnrollments = async (search: string, page: number, top: number, studentId?: string, courseBranchId?: string) => {
+export const getEnrollments = async (search: string, page: number, top: number, studentId?: string, courseBranchId?: string, enrollmentDate?: string) => {
     const skip = (page - 1) * top;
 
 
@@ -21,6 +21,17 @@ export const getEnrollments = async (search: string, page: number, top: number, 
     }
     if(courseBranchId){
         where.courseBranchId = { equals: courseBranchId };
+    }
+
+    // Filtrar por enrollmentDate
+    if (enrollmentDate) {
+        const date = new Date(enrollmentDate);
+        if (!isNaN(date.getTime())) {
+            where.enrollmentDate = {
+                gte: new Date(date.setHours(0, 0, 0, 0)), // Desde las 00:00
+                lte: new Date(date.setHours(23, 59, 59, 999)) // Hasta las 23:59
+            };
+        }
     }
 
 
