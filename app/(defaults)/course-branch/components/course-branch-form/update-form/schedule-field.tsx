7ea155db6dff70b2de3f2a1cb81a9e8983e25732
Anderson Frias <anderl.friaas@gmail.com>
2025-02-24 17:57:38 -0400

    [Add] Inicio de la vista de agregar horarios a course-branch

--- app/(defaults)/course-branch/components/course-branch-form/update-form/schedule-field.tsx
+++ app/(defaults)/course-branch/components/course-branch-form/update-form/schedule-field.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { CourseBranchFormType } from '../form.config';
 import { FormikErrors, FormikTouched } from 'formik';
-import { FormItem } from '@/components/ui';
 import useFetchSchedule from '@/app/(defaults)/schedules/lib/use-fetch-schedules';
+import { Schedule } from '@prisma/client';
 
 interface ScheduleFieldProps {
   values: CourseBranchFormType;
@@ -11,14 +11,38 @@ interface ScheduleFieldProps {
   className?: string;
 }
 
+function groupSchedulesByWeekday(schedules: Schedule[]): { [key: number]: Omit<Schedule, 'weekday' | 'deleted' | 'createdAt' | 'updatedAt'>[] } {
+  const groupedByWeekday: { [key: number]: Omit<Schedule, 'weekday' | 'deleted' | 'createdAt' | 'updatedAt'>[] } = {};
+
+  schedules.forEach(schedule => {
+    if (!schedule.deleted) {
+      const weekday = schedule.weekday;
+
+      if (!groupedByWeekday[weekday]) {
+        groupedByWeekday[weekday] = [];
+      }
+
+      groupedByWeekday[weekday].push({
+        id: schedule.id,
+        startTime: schedule.startTime,
+        endTime: schedule.endTime
+      });
+    }
+  });
+
+  return groupedByWeekday;
+}
+
 export default function ScheduleField({ values, errors, touched, className }: ScheduleFieldProps) {
   const { schedules } = useFetchSchedule();
   console.log('schedules', schedules);
+  console.log('groupedSchedules', groupSchedulesByWeekday(schedules));
+
   return (
     <div className={className}>
       <div className=''>
         <label htmlFor="fullName">Horarios </label>
-        
+
       </div>
     </div>
   )
