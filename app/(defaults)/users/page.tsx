dd31b029d41420d118a1194e84d062c5cdee2153
Wilber Galvez <galvezwilber22@gmail.com>
2025-03-23 16:14:57 -0400

    [fix] conflict wirh page users

---
+++ app/(defaults)/users/page.tsx
@@ -0,0 +1,37 @@
+import { Metadata } from "next";
+import ViewTitle from "@/components/common/ViewTitle";
+import IconUserPlus from "@/components/icon/icon-user-plus";
+import Button from "@/components/ui/button";
+import Link from "next/link";
+import { SearchInput } from "@/components/common";
+import { objectToQueryString } from "@/utils";
+import UserList from "./components/user-list";
+
+export const metadata: Metadata = {
+    title: 'Usuarios',
+};
+
+interface UsersProps {
+    searchParams?: {
+        search?: string;
+        page?: string;
+    };
+}
+
+export default function Users({ searchParams }: UsersProps) {
+    const query = objectToQueryString(searchParams || {});
+    return (
+        <div>
+            <ViewTitle className='mb-6' title="Usuarios" rightComponent={
+                <>
+                    <SearchInput placeholder="Buscar usuarios" />
+                    <Link href="/users/new">
+                        <Button icon={<IconUserPlus />}>Crear usuario</Button>
+                    </Link>
+                </>
+            } />
+
+            <UserList query={query} />
+        </div>
+    );
+}
