ffb5d509d1bd32d5a2a977862b2e81ad9cadf681
Wilber Galvez <galvezwilber22@gmail.com>
2025-04-04 08:59:41 -0400

    [update] formulario de productos

---
+++ app/(defaults)/products/components/product-forms/form.config.ts
@@ -0,0 +1,33 @@
+
+import * as Yup from 'yup';
+
+
+export const createValidationSchema = Yup.object().shape({
+    code: Yup.string().required('El código es requerido'),
+    name: Yup.string().required('El nombre es requerido'),
+    description: Yup.string(),
+    cost : Yup.number().required('El costo es requerido').positive('El costo debe ser un número positivo'),
+    price: Yup.number().required('El precio es requerido').positive('El precio debe ser un número positivo'),
+    stock: Yup.number().required('El stock es requerido').integer('El stock debe ser un número entero').positive('El stock debe ser un número positivo'),
+    isTaxIncluded: Yup.boolean(),
+});
+
+export const updateValidationSchema = Yup.object().shape({
+    code: Yup.string().required('El código es requerido'),
+    name: Yup.string().required('El nombre es requerido'),
+    description: Yup.string(),
+    cost : Yup.number().required('El costo es requerido').positive('El costo debe ser un número positivo'),
+    price: Yup.number().required('El precio es requerido').positive('El precio debe ser un número positivo'),
+    stock: Yup.number().required('El stock es requerido').integer('El stock debe ser un número entero').positive('El stock debe ser un número positivo'),
+    isTaxIncluded: Yup.boolean(),
+});
+
+export const initialValues = {
+    code: '',
+    name: '',
+    description: '',
+    cost: 0,
+    price: 0,
+    stock: 0,
+    isTaxIncluded: false,
+};
