e0bbfe152d74417119e13e2342b38f2510f0a5a1
Wilber Galvez <galvezwilber22@gmail.com>
2025-04-04 09:00:05 -0400

    [add] vista de productos

---
+++ app/(defaults)/products/page.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import ViewTitle from "@/components/common/ViewTitle";
+import Button from "@/components/ui/button";
+import Link from "next/link";
+import { SearchInput } from "@/components/common";
+import { objectToQueryString } from "@/utils";
+import { IconPlusCircle } from "@/components/icon";
+import ProductList from "./components/product-list";
+
+export const metadata: Metadata = {
+    title: 'Productos',
+};
+
+interface ProductProps {
+    searchParams?: {
+        search?: string;
+        page?: string;
+    };
+}
+
+export default function Product({ searchParams }: ProductProps) {
+    const query = objectToQueryString(searchParams || {});
+    return (
+        <div>
+            <ViewTitle className='mb-6' title="Productos" rightComponent={
+                <>
+                    <SearchInput placeholder="Buscar Productos" />
+                    <Link href="/products/new">
+                        <Button icon={<IconPlusCircle />}>Crear promoción</Button>
+                    </Link>
+                </>
+            } />
+
+            <ProductList query={query} />
+        </div>
+    );
+}
