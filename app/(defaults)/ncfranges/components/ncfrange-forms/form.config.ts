f19c619f2f3febde0a22211623acbae111887574
Wilber Galvez <galvezwilber22@gmail.com>
2025-04-07 09:06:35 -0400

    [update] modificando form config de ncf

--- app/(defaults)/ncfranges/components/ncfrange-forms/form.config.ts
+++ app/(defaults)/ncfranges/components/ncfrange-forms/form.config.ts
@@ -11,10 +11,8 @@ export const createValidationSchema = Yup.object().shape({
     .required('El tipo es requerido'),
     startSequence: Yup.number().required('La secuencia inicial es requerida').integer('La secuencia inicial debe ser un número entero').positive('La secuencia inicial debe ser un número positivo'),
     endSequence: Yup.number().required('La secuencia final es requerida').integer('La secuencia final debe ser un número entero').positive('La secuencia final debe ser un número positivo'),
-    currentSequence: Yup.number().required('La secuencia actual es requerida').integer('La secuencia actual debe ser un número entero').positive('La secuencia actual debe ser un número positivo'),
     dueDate: Yup.date().required('La fecha de vencimiento es requerida').nullable(),
     authorizationNumber: Yup.string().required('El número de autorización es requerido'),
-    isActive: Yup.boolean(),
 
 });
 
@@ -24,19 +22,16 @@ export const updateValidationSchema = Yup.object().shape({
     .required('El tipo es requerido'),
     startSequence: Yup.number().required('La secuencia inicial es requerida').integer('La secuencia inicial debe ser un número entero').positive('La secuencia inicial debe ser un número positivo'),
     endSequence: Yup.number().required('La secuencia final es requerida').integer('La secuencia final debe ser un número entero').positive('La secuencia final debe ser un número positivo'),
-    currentSequence: Yup.number().required('La secuencia actual es requerida').integer('La secuencia actual debe ser un número entero').positive('La secuencia actual debe ser un número positivo'),
     dueDate: Yup.date().required('La fecha de vencimiento es requerida').nullable(),
     authorizationNumber: Yup.string().required('El número de autorización es requerido'),
-    isActive: Yup.boolean(),
+
 });
 
 export const initialValues = {
-    prefix: '',
+    prefix: 'B',
     type: null,
     startSequence: 0,
     endSequence: 0,
-    currentSequence: 0,
     dueDate: null,
-    isActive: true,
     authorizationNumber: '',
 };
