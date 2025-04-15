823669892995c0a8fc613373e964d9c206b988c2
Wilber Galvez <galvezwilber22@gmail.com>
2025-04-15 11:46:15 -0400

    [uppate] quitando componentes para llevarlo en un layout

--- app/(defaults)/invoices/[id]/page.tsx
+++ app/(defaults)/invoices/[id]/page.tsx
@@ -1,30 +1,13 @@
 'use client';
-import { FormSkeleton, ViewTitle } from "@/components/common";
-import { useFetchCashRegistersById } from "../lib/use-fetch-cash-register";
-import CashRegisterDetails from "../components/cash-register/cash-register-details";
-
-
-
-
-
-
 
 export default function EditCashRegister({ params }: { params: { id: string } }) {
-    const { id } = params;
-    const { loading, CashRegister } = useFetchCashRegistersById(id);
+    
    
     return (
         <div>
-            <ViewTitle className='mb-6' title="Facturacion" showBackPage />
+           
             
 
-            {loading && <FormSkeleton />}
-            
-            {CashRegister && (
-                <>
-                <CashRegisterDetails CashRegister={CashRegister} />
-                </>
-            ) }
         </div>
     )
 }
