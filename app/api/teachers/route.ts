4c8e8012a063c6d0d56b7bbc4a5538f61556dd07
Anderson Frias <anderl.friaas@gmail.com>
2025-07-31 16:14:32 -0400

    [Update] Limpieza de codigo

--- app/api/teachers/route.ts
+++ app/api/teachers/route.ts
@@ -25,7 +25,6 @@ export async function GET(request: NextRequest) {
 export async function POST(request: Request) {
     try {
         const body = await request.json();
-        console.log('BODY: ',body);
 
         // Validate the request body
         const {isValid, message} = validateObject(body, ["firstName", "lastName", "identification"]);
