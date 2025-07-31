ecc799c9b08657c7be7cce281e65954342ac13a6
Anderson Frias <anderl.friaas@gmail.com>
2025-07-31 01:42:12 -0400

    [Update] Cambio minimo de diseño

--- app/(defaults)/course-branch/components/course-branch-details/course-branch-details.tsx
+++ app/(defaults)/course-branch/components/course-branch-details/course-branch-details.tsx
@@ -44,46 +44,6 @@ export default function CourseBranchDetails({ courseBranch }: { courseBranch: Co
                     <span className="text-gray-700 dark:text-gray-200 font-semibold break-words">{value || "No especificado"}</span>
                 </div>
             ))}
-
-            {/* <div className="grid grid-cols-1 sm:grid-cols-3 col-span-2 gap-2">
-                <Tooltip title="Eliminar">
-                    <Button
-                        onClick={() => onDelete(courseBranch.id)}
-                        variant="outline"
-                        size="sm"
-                        icon={<IconTrashLines className="size-4" />}
-                        color="danger"
-                        className="w-full"
-                    >
-                        Eliminar
-                    </Button>
-                </Tooltip>
-                <Tooltip title="Editar">
-                    <Link href={`/course-branch/${courseBranch.id}`} className="w-full">
-                        <Button
-                            variant="outline"
-                            size="sm"
-                            icon={<IconEdit className="size-4" />}
-                            className="w-full"
-                        >
-                            Editar
-                        </Button>
-                    </Link>
-                </Tooltip>
-                <Tooltip title="Inscribir">
-                    <Link href={`/enrollments/new?courseBranchId=${courseBranch.id}`} className="w-full">
-                        <Button
-                            variant="outline"
-                            size="sm"
-                            icon={<IconUserPlus className="size-4" />}
-                            className="w-full "
-                            color='success'
-                        >
-                            Inscribir
-                        </Button>
-                    </Link>
-                </Tooltip>
-            </div> */}
         </div>
 
 
