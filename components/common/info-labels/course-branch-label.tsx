d1a31fb94217dbe0d2d6230e89addbce66bc782a
Wilber Galvez <galvezwilber22@gmail.com>
2025-07-31 08:35:07 -0400

    [update] validacion si no encuentra datos

--- components/common/info-labels/course-branch-label.tsx
+++ components/common/info-labels/course-branch-label.tsx
@@ -4,6 +4,7 @@ import apiRequest from '@/utils/lib/api-request/request';
 import ModalityTag from '@/app/(defaults)/course-branch/components/modality';
 import { formatSchedule } from '@/utils/schedule';
 import { CourseBranch } from '@/app/(defaults)/course-branch/lib/use-fetch-course-branch';
+import OptionalInfo from '../optional-info';
 
 type Props = {
   CourseBranchId: string;
@@ -14,21 +15,42 @@ type Props = {
 
 export default function CourseBranchLabel({ CourseBranchId, isSelected, showTeacher = true }: Props) {
   const [courseBranch, setCourseBranch] = useState<CourseBranch | null>(null);
-
+  const [noData, setNoData] = useState(false);
   useEffect(() => {
     const fetchCourseBranchById = async () => {
       try {
         const response = await apiRequest.get<CourseBranch>(`/course-branch/${CourseBranchId}`);
-        if (response.success && response.data) {
-          console.log('response course branch', response);
-          setCourseBranch(response.data);
+        console.log('data course branch', response)
+        const data = response.data;
+
+        if (
+          response.success &&
+          data &&
+          typeof data === 'object' &&
+          Object.keys(data).length > 0
+        ) {
+          setCourseBranch(data);
+          setNoData(false);
+        } else {
+          setNoData(true);
+          setCourseBranch(null);
         }
       } catch (error) {
         console.error('Error fetching single courseBranch:', error);
+        setNoData(true); // podrías separar errores reales si lo deseas
       }
     };
+
     fetchCourseBranchById();
   }, [CourseBranchId]);
+
+  if (noData) {
+    return (
+      <div className="p-3">
+        <OptionalInfo message="No se encontraron datos del curso." />
+      </div>
+    );
+  }
 
   return courseBranch ? (
     <div
