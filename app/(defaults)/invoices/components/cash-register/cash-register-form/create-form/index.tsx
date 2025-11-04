5afcb230ae0aa6eb748a922d9760141c2fa577c4
Anderson Frias <anderl.friaas@gmail.com>
2025-11-04 10:56:20 -0400

    feat: set initial balance field value to an empty string when undefined

--- app/(defaults)/invoices/components/cash-register/cash-register-form/create-form/index.tsx
+++ app/(defaults)/invoices/components/cash-register/cash-register-form/create-form/index.tsx
@@ -105,6 +105,7 @@ export default function CreateCashRegisterForm() {
                         >
                             <Field
                                 type="number"
+                                value={values.initialBalance || ''}
                                 onWheel={(e: React.WheelEvent<HTMLInputElement>) => (e.target as HTMLInputElement).blur()}
                                 name="initialBalance"
                                 component={Input}
