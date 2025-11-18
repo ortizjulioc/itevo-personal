69f165262c401e81f2ffc5f26c166fe934112d10
Anderson Frias <anderl.fraas@gmail.com>
2025-11-18 01:37:35 +0000

    add: configuracion para dezplegar a produccion

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,14 @@
 module.exports = {
   apps: [{
-    name: 'software-itevo-app',
+    name: 'itevo-app',
     script: 'npm',
     args: ["start"],
     autorestart: true,
     env: {
       NODE_ENV: 'production',
+      PORT: 1441,
       PM2_SERVE_PATH: './build',
-      PM2_SERVE_PORT: 3000,
+      PM2_SERVE_PORT: 1441,
       PM2_SERVE_SPA: 'true',
       PM2_SERVE_HOMEPAGE: '/index.html'
     }
