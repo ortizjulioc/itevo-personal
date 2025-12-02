7d06b43c0dbd98c38cfe17333bf34a6bd89cdf5a
Anderson Frias <anderl.friaas@gmail.com>
2025-12-02 09:52:15 -0400

    feat: update invoice API to include detailed cashBox information and adjust print logic for invoice details

--- app/api/invoices/[id]/route.ts
+++ app/api/invoices/[id]/route.ts
@@ -20,7 +20,9 @@ export async function GET(_: NextRequest, { params }: { params: { id: string } }
         cashRegister: {
           select: {
             id: true,
-            cashBox: true,
+            cashBox: {
+              select: { id: true, name: true, branch: true}
+            },
             user: {
               select: {
                 id: true,
