e57f5e75a66ccf29f07b44228f07a287f928bd46
Anderson Frias <anderl.friaas@gmail.com>
2026-03-11 11:20:37 -0400

    fix: se soluciono problema de conexion con maria db

--- utils/lib/prisma/index.ts
+++ utils/lib/prisma/index.ts
@@ -2,13 +2,20 @@ import 'dotenv/config';
 import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 import { PrismaClient } from '@/generated/prisma/client';
 
+const databaseUrlString = process.env.DATABASE_URL;
+if (!databaseUrlString) {
+    throw new Error('❌ DATABASE_URL no está definido en las variables de entorno');
+}
+
+const dbUrl = new URL(databaseUrlString);
+
 const prismaClientSingleton = () => {
     const adapter = new PrismaMariaDb({
-        host: process.env.DATABASE_HOST,
-        port: Number(process.env.DATABASE_PORT),
-        user: process.env.DATABASE_USER,
-        password: process.env.DATABASE_PASSWORD,
-        database: process.env.DATABASE_NAME,
+        host: dbUrl.hostname,
+        port: dbUrl.port ? parseInt(dbUrl.port, 10) : 3306,
+        user: dbUrl.username,
+        password: dbUrl.password,
+        database: dbUrl.pathname.slice(1),
         connectionLimit: 5,
     });
 
