6e6e04c8168c37ea8e2d4b89e62034367e0973ca
Anderson Frias <anderl.friaas@gmail.com>
2026-03-11 12:01:40 -0400

    update: se puso opcional el correo y el numero de telefono en los formularios de sucursales

--- app/(defaults)/branches/components/branch-forms/form.config.ts
+++ app/(defaults)/branches/components/branch-forms/form.config.ts
@@ -1,20 +1,17 @@
-
 import * as Yup from 'yup';
-
 
 export const createValidationSchema = Yup.object().shape({
     name: Yup.string().required('El nombre es obligatorio'),
     address: Yup.string().required('La direccion es obligatoria'),
-    phone: Yup.string(),
-    email: Yup.string().email('Correo electrónico inválido'),
-   
+    phone: Yup.string().optional(),
+    email: Yup.string().email('Correo electrónico inválido').optional(),
 });
 
 export const updateValidationSchema = Yup.object().shape({
     name: Yup.string().required('El nombre es obligatorio'),
     address: Yup.string().required('La direccion es obligatoria'),
-    phone: Yup.string(),
-    email: Yup.string().email('Correo electrónico inválido'),
+    phone: Yup.string().optional(),
+    email: Yup.string().email('Correo electrónico inválido').optional(),
 });
 
 export const initialValues = {
