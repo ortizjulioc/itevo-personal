61cb49d086c0c3c3aaf9c6eabfa8c8f2b7cbddb5
Vladimir Nuñez <vnc3xp@gmail.com>
2026-03-11 12:33:00 -0400

    feat: implement initial dashboard with key metrics, sales charts, and recent transactions.

--- app/(defaults)/page.tsx
+++ app/(defaults)/page.tsx
@@ -3,17 +3,19 @@ import React from 'react';
 import { getServerSession } from "next-auth/next";
 import ViewTitle from "@/components/common/ViewTitle";
 import { authOptions } from '../api/auth/[...nextauth]/auth-options';
+import DashboardClient from '@/components/dashboard/DashboardClient';
 
 export const metadata: Metadata = {
-    title: 'Inicio',
+    title: 'Dashboard de Inicio',
 };
 
 const Sales = async () => {
     const session = await getServerSession(authOptions);
 
     return (
-        <div>
+        <div className="space-y-6">
             <ViewTitle title={`¡Bienvenido ${session?.user?.name} ${session?.user?.lastName}!`} />
+            <DashboardClient />
         </div>
     );
 };
