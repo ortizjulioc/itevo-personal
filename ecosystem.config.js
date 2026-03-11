d75a3100731f65b7d13a50357b4820a7c861beac
Anderson Frias <anderl.friaas@gmail.com>
2026-03-11 11:22:19 -0400

    update: se modifico el nombre de la app en el ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,16 @@
 module.exports = {
-  apps: [{
-    name: 'itevo-norvis-y-agraciado',
-    script: 'npm',
-    args: ["start"],
-    autorestart: true,
-    env: {
-      NODE_ENV: 'production',
-      PORT: 1442,
-      PM2_SERVE_PATH: './build',
-      PM2_SERVE_PORT: 1442,
-      PM2_SERVE_SPA: 'true',
-      PM2_SERVE_HOMEPAGE: '/index.html'
-    }
-  }]
+    apps: [{
+        name: 'itevo-nya',
+        script: 'npm',
+        args: ["start"],
+        autorestart: true,
+        env: {
+            NODE_ENV: 'production',
+            PORT: 1442,
+            PM2_SERVE_PATH: './build',
+            PM2_SERVE_PORT: 1442,
+            PM2_SERVE_SPA: 'true',
+            PM2_SERVE_HOMEPAGE: '/index.html'
+        }
+    }]
 }
