ae9a2cf3943b4190de2c7d280cd24cad6b3692df
Anderson Frias <anderl.friaas@gmail.com>
2026-03-18 10:47:16 -0400

    add: se agrego el nombre de la app en el footer al .env

--- components/layouts/footer.tsx
+++ components/layouts/footer.tsx
@@ -1,7 +1,8 @@
+
 const Footer = () => {
     return (
         <div className="p-6 pt-0 mt-auto text-center dark:text-white-dark ltr:sm:text-left rtl:sm:text-right">
-            © {new Date().getFullYear()}. ITEVO NORVIS DE LEON & AGRACIADO RODRIGUEZ | Todos los derechos reservados.
+            © {new Date().getFullYear()}. {process.env.APP_NAME} | Todos los derechos reservados.
         </div>
     );
 };
